73907219393a41ce14af824ec298ad5df6ef6919
akkhilaysh <akkhilaysh@github.com>
2016-11-14 10:12:59 -0500

    Made changes to server.js, main.js and style

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/liv.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'liv.png'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+//Change text of main-text div
+
+var element = document.getElementById('main-text');
+element.innerHTML = "Hi, I'm Akkhilaysh. Welcome to my personal profile!";
+
+
+
+	
